13d9b7332423ee4b2ee9fb05e02b25df25f6c65f
Junior Ramirez <Juniorr87@yahoo.com>
2015-05-19 13:29:50 -0700

    trying to merge code on to both computers -Ramon

---
+++ screens/mainmenu.js
@@ -0,0 +1,60 @@
+game.mainmenu = me.ScreenObject.extend({      
+        /**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('asap-mob')), -10); // TODO
+
+        
+        me.game.world.addChild(new (me.Renderable.extend({
+                   init:function(){
+                       this._super(me.Renderable, 'init', [270, 240, 400, 50]);
+                       this.font = new me.Font("Arial", 46, "white");
+                       me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                   },
+                   
+                   draw: function(renderer){
+                       this.font.draw(renderer.getContext(), "ASVP", this.pos.x, this.pos.y);
+                   },
+                   
+                   update:function(dt){
+                       return true;
+                   },
+                   
+                   newGame: function(){
+                       me.input.releasePointerEvent('pointerdown', this);
+                       me.state.change(me.state.NEW);
+                   }
+            })));
+           
+             me.game.world.addChild(new (me.Renderable.extend({
+                   init:function(){
+                       this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+                       this.font = new me.Font("Arial", 46, "white");
+                       me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                   },
+                   
+                   draw: function(renderer){
+                       this.font.draw(renderer.getContext(), "TDE", this.pos.x, this.pos.y);
+                   },
+                   
+                   update:function(dt){
+                       return true;
+                   },
+                   
+                   newGame: function(){
+                         me.input.releasePointerEvent('pointerdown', this);
+                        me.state.change(me.state.LOAD);
+                   }
+            })));
+            
+            },
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		
+        }
+});
